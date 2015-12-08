0737751f861f0d8685a159dfad5e1649835a3fe0
Oleg Charyshkin <o.charyshkin@gmail.com>
2015-12-08 09:45:03 +0300

    * list catalog categories in the left navigation pane

---
+++ src/app/catalog.js
@@ -0,0 +1,89 @@
+angular.module('flakio')
+  .controller('Catalog',function(catalogService) {
+    var self = this;
+    self.selectedCategoryId = 0;
+
+    catalogService.catalog().then(function(catalog){
+      self.allCatalogItems = catalog;
+      self.catalog = catalog;
+    });
+
+    catalogService.categories().then(function(categories){
+      var tmpCategories = categories.slice(0);
+      tmpCategories.splice(0, 0, {id: 0, name: 'Everything'})
+      self.categories = tmpCategories;
+    });
+
+    self.selectCategory = function(category){
+      self.selectedCategoryId = category.id;
+      if (!self.allCatalogItems){
+        return;
+      }
+      //todo: figure out item category relationship
+      if (category.id === 0){
+        self.catalog = self.allCatalogItems;
+        return;
+      }
+      self.catalog = self.allCatalogItems.filter(function(x){
+        return x.name === category.name;
+      });
+    }
+  })
+
+  .service('catalogService', function($q, $http) {
+    var self = this;
+
+    this.categories = function(){
+      var deferred = $q.defer();
+
+      deferred.resolve( [{id: '1',
+        name: 'Lazer Blaster'},
+        {id: '2',
+          name: 'Pet Space Rock'},
+        {id: '3',
+          name: 'New World Flag Pole'} ]);
+
+      return deferred.promise;
+
+      $http({
+        method: 'GET',
+        url: '/api/categories'
+      }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+    };
+
+    this.catalog = function() {
+      var deferred = $q.defer();
+
+      deferred.resolve( [{id: '1',
+        name: 'Lazer Blaster',
+        description: 'Don\'t get stuck on mars without it.',
+        imagePath: 'https://flak.blob.core.windows.net/catalog/lazer.jpg'},
+        {id: '2',
+          name: 'Pet Space Rock',
+          description: 'Just don\'t feed it after midnight.',
+          imagePath: 'https://flak.blob.core.windows.net/catalog/pet-bio-rock.jpg'},
+        {id: '3',
+          name: 'New World Flag Pole',
+          description: 'Claim that new world you found.',
+          imagePath: 'https://flak.blob.core.windows.net/catalog/indestructable-high-suction-flag-pole.jpg'} ]);
+
+      return deferred.promise;
+
+      $http({
+        method: 'GET',
+        url: '/api/catalog'
+      }).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+    }
+  });
